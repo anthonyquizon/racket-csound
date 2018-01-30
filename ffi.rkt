271f8f67326b9377e8f45189bf20943317cf1f47
Anthony Quizon <anthoq88@gmail.com>
2018-01-30 13:47:59 +1100

    ffi

---
+++ ffi.rkt
@@ -0,0 +1,56 @@
+#lang racket
+
+(provide csound-initialize csound-create csound-set-option 
+         csound-compile csound-compile-orc csound-read-score
+         csound-start csound-perform csound-perform-ksmps 
+         csound-stop)
+        
+(require racket/system
+         ffi/unsafe
+         ffi/unsafe/define)
+
+(define cs-lib-osx-local-framework 
+  (string-append (path->string (find-system-path 'home-dir)) "/Library/Frameworks/CsoundLib64.framework/CsoundLib64"))
+(define cs-lib-osx-global-framework "/Library/Frameworks/CsoundLib64.framework/CsoundLib64")
+(define cs-lib-osx-brew "/usr/local/Cellar/csound/6.10.0/lib/libcsnd6.dylib")
+(define cs-lib-default "libcsound64") 
+(define cs-lib  
+  (cond 
+    [(file-exists? cs-lib-osx-local-framework) cs-lib-osx-local-framework ]
+    [(file-exists? cs-lib-osx-global-framework) cs-lib-osx-global-framework ]
+    [(file-exists? cs-lib-osx-brew) cs-lib-osx-brew]
+    [else cs-lib-default ]))
+  
+(define-ffi-definer define-csound (ffi-lib cs-lib))
+
+(define _CSOUND-pointer (_cpointer 'CSOUND))
+
+
+(define-csound csoundInitialize (_fun _int -> _void))
+(define-csound csoundCreate (_fun -> _CSOUND-pointer))
+(define-csound csoundSetOption (_fun _CSOUND-pointer _string -> _void))
+(define-csound csoundCompileOrc (_fun _CSOUND-pointer _string -> _void))
+(define-csound csoundReadScore (_fun _CSOUND-pointer _string -> _void))
+
+(define-csound csoundCompile (_fun _CSOUND-pointer _int  (_array/list _string 2) -> _int))
+(define-csound csoundStart (_fun _CSOUND-pointer -> _int))
+(define-csound csoundPerform (_fun _CSOUND-pointer -> _int))
+(define-csound csoundPerformKsmps (_fun _CSOUND-pointer -> _int))
+(define-csound csoundStop (_fun _CSOUND-pointer -> _void))
+
+
+(define (csound-initialize flags)
+  (csoundInitialize flags))
+
+(define csound-create csoundCreate)
+(define csound-set-option csoundSetOption)
+
+(define csound-compile-orc csoundCompileOrc)
+(define csound-read-score csoundReadScore)
+(define (csound-compile cs args)
+  (csoundCompile cs (length args) args))
+
+(define csound-start csoundStart)
+(define csound-perform csoundPerform)
+(define csound-perform-ksmps csoundPerformKsmps)
+(define csound-stop csoundStop)
--- main.rkt
+++ main.rkt
@@ -1,35 +1,36 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+(require "ffi.rkt")
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included a LICENSE.txt file, which links to
-;; the GNU Lesser General Public License.
-;; If you would prefer to use a different license, replace LICENSE.txt with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
 
-;; Code here
+(csound-initialize 3)
 
-(module+ test
-  ;; Tests to be run with raco test
-  )
+(define orc #<<EOF
+sr=44100
+ksmps=32
+nchnls=2
+0dbfs=1
 
-(module+ main
-  ;; Main entry point, executed when run with the `racket` executable or DrRacket.
-  )
+instr 1 
+aout vco2 0.5, 440
+outs aout, aout
+endin
+EOF
+)
+
+
+; Defining our Csound SCO code 
+(define sco "i1 0 1")
+
+(let ([cs (csound-create)] ; Create an instance of the Csound object 
+      [args '("csound" "test1.csd")]) ; Create args as list
+
+  (csound-set-option cs "-odac") ; Using SetOption() to configure Csound 
+                                 ; Note: use only one commandline flag at a time
+  (csound-compile-orc cs orc)     ; Compile the Csound Orchestra String
+  (csound-read-score cs sco)      ; Compile the Csound SCO String 
+  (csound-start cs)               ; When compiling from strings, this call is necessary before doing any performing 
+  (csound-perform cs)            ; This call runs Csound to completion
+  (csound-stop cs))              ; At this point, Csound is already stopped, but this call is here
+                                 ; as it is something that you would generally call in real-world 
+                                 ; contexts 
