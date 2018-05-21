55a35bf83a843e14c14b912f0b2bf597e971cebf
Anthony Quizon <anthoq88@gmail.com>
2018-05-21 18:33:12 +1000

    added score form

--- examples/basic.rkt
+++ examples/basic.rkt
@@ -1,7 +1,5 @@
 #lang racket
 
-(define a 
-  (instr (oscils a1 1000 550 1) 
-         (out a1)))
+(define a (oscils a1 1000 550 1))
 
 (a 0 10)
--- private/compiler.rkt
+++ private/compiler.rkt
@@ -1,29 +1,50 @@
 #lang nanopass
 
+(module+ test 
+  (require rackunit))
+
 (define primitives '(oscil))
 
 (define (Primitive? v) (memq v primitives))
 (define (Id? v) (number? v))
+(define (PValue? v) (number? v))
 (define (Out? v) (equal? v 'out))
 (define (Argument? v) (number? v))
 (define (Variable? v) ;;TODO control or audio rate 
   (and (symbol? v) (not (Primitive? v)))) 
 
-(define-language LINST 
-  (entry Instr)
+;;pass define sig
+;;pass define inst
+;;pass automatic instr id
+
+;(define-language L1
+  ;(extends L0)
+  ;(terminals 
+    ;(- (Id (i))))
+  ;(Stmt (S)
+    ;(stmt v p a1 ...)))
+
+(define-language L0 
+  (entry CSD)
   (terminals 
     (Argument (a))
-    (Id (i))
+    (PValue (pv))
+    (Id (id))
     (Variable (v))
     (Out (o))
     (Primitive (p)))
   (Stmt (S)
     (stmt v p a1 ...))
   (Instr (I)
-    (instr i S1 ... (o v)))
-  )
+    (instr id S1 ... (o v)))
+  (Event (E)
+    (i pv1 pv2 pv3 pv* ...))
+  (Score (Sc)
+    (score E1 ...))
+  (CSD (C)
+    (csd I Sc)))
 
-(define-pass pass-instr : LINST (ir) -> * ()
+(define-pass pass-csd : L0 (ir) -> * ()
   (definitions 
     (define (~string-list xs sep)
       (apply string-append 
@@ -33,27 +54,46 @@
       (format "~a ~a ~a" var pr (~args args)))
     (define (~instr id stmts v) 
       (define stmts^ (~string-list stmts "\n"))
-      (format "instr ~a \n ~a \n out ~a" id stmts^ v)))
+      (format "instr ~a \n ~a \n out ~a" id stmts^ v))
+    (define (~i pv1 pv2 pv3 pv*)
+      (format "i ~a ~a ~a ~a" pv1 pv2 pv3 (~string-list pv* " ")))
+    (define (~score events)
+      (~string-list events "\n"))) 
+
   (Stmt : Stmt (S) -> * ()
     [(stmt ,v ,p ,a1 ...) (~stmt v p a1)])
   (Instr : Instr (I) -> * ()
-    [(instr ,i ,S1 ... (,o ,v))
+    [(instr ,id ,S1 ... (,o ,v))
      (define str-S1 (map Stmt S1))
-     (~instr i str-S1 v)])
-  )
+     (~instr id str-S1 v)])
+  (Event : Event (E) -> * ()
+    [(i ,pv1 ,pv2 ,pv3 ,pv* ...) (~i pv1 pv2 pv3 pv*)])
+  (Score : Score (Sc) -> * ()
+    [(score ,E1 ...) 
+     (define str-E1 (map Event E1))
+     (~score str-E1)])
+  (CSD : CSD (C) -> * ()
+    [(csd ,I ,Sc)
+     `(,(Instr I) . ,(Score Sc))])) 
 
-(define-parser parse LINST)
+(define-parser parse L0)
 
-;(define (parse s)
-  ;(with-output-language (LINST Stmt)
-    ;(cond
-      ;[(symbol? s) `,s]
-      ;[else (error)])))
+(module+ test
+ 
+   (define ex '(csd
+                 (instr 1 
+                        (stmt aSin oscil 100 200) 
+                        (out aSin))
+                 (score 
+                   (i 1 0 0)
+                   (i 1 200 10))))
 
-(pass-instr 
-  (parse 
-    '(instr 1 (stmt aSin oscil 100 200) 
-              (out aSin))))
+   (pass-csd (parse ex)))
 
-;(pass-instr (parse '(stmt aSin oscil 100 200)))
+   ;(check-equal? 
+     ;(pass-csd 
+       ;(parse ex)
+       ;"instr 1\n aSin")))
+ 
+ 
 
